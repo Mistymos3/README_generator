0e2cfd5ab2f9cdad88293d3a10a2dad50d98b81c
Iva <84078385+Mistymos3@users.noreply.github.com>
2021-09-15 12:44:43 -0700

    added code

--- Develop/index.js
+++ Develop/index.js
@@ -2,19 +2,92 @@ const fs = require("fs");
 const path = require("path");
 const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown");
+const { rejects } = require("assert");
 
 // array of questions for user
+///array of Qs for inquirer
 const questions = [
+    {
+        type: "input",
+        name: "github",
+        message: "What is your GitHub username?"
+    },
+
+    {  
+        type: "input",
+        name: "email",
+        message: "What is your eMail address?"
+    },
+
+    {
+        type:"input",
+        name: "title",
+        message: "What is your project title?"
+    
+    },
+    
+    {
+        type:"input",
+        name:"description",
+        message:"What is your project description?"
+    },
+        
+    {
+        type:"input",
+        name:"installation",
+        message:"What are the installation instructions?"
+    },
+   
+    {
+        type:"input",
+        name:"tests",
+        message:"Which tests were used?"
+    },
+    
+    {
+        type:"input",
+        name:"contribution",
+        message:"How can users contribute?"
+    },
+    
+    {
+        type:"list",
+        name:"license",
+        message:"Which licenses do you want to use?",
+        choices: [
+            "MIT", "Apache 2.0", "GPL 3.0", "BSD 3", "none"
+        ]
+        },   
+
 
 ];
 
 // function to write README file
-function writeToFile(fileName, data) {
+function writeToFile(fileName, data, path) {
+    
+    //tdb fs.writeFileSync( ..., fileName, data)
+    fs.writeFileSync(path(fileName), data, path => {
+        if (path) {
+            rejects(path);
+            return;
+        }
+    });
 }
+
 
 // function to initialize program
 function init() {
-
+    ///when call inquirer.prompt it'll run through all questions then 
+    inquirer.prompt(questions)
+    ///then whatever you call the answers are called here (inquirerResponse)..for now. 
+                            ///then take the answers and pipe them into a callback
+    .then((inquirerResponses) => {
+        ///what i want to do with them
+        console.log("Generate Readme");
+        ///then invoke(call) writeToFile which will take the data and fileName
+        ///calling it with the word README b/c thats the name of the file that'll be generated
+        writeToFile("README.md", generateMarkdown({...inquirerResponses}));
+    })
 }
 
 // function call to initialize program
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,7 +1,110 @@
 // function to generate markdown for README
+
+
+///only if license is not equal to none will this function be called
+function renderLicenseBadge(license){
+  ///if license not equal to none means they selected a license
+  if (license !== "none") {
+    return (
+      `## Project Description
+      ${description}
+      `
+      ///return
+      //inbetween ticks
+      //headline
+      //then a sentence. tick is instead of concatinating with +
+      // `## License
+      
+      // This project is licensed under the ${license} license.`
+  )}
+  return `## Description
+          No description provided`
+}
+
+
+// function renderTableOfContents(tableOfContents){
+//   if(tableOfContents!=="none"){
+//     return(
+//       `## Table of Contents
+//       [Project Description](#Project-Description)
+//       [Installation Instructions](#Installation-Instructions)
+//       [Tests](#Tests)
+//       [Contribution](#Contribution) 
+//       [License](#License)
+//       [Github](#Github)
+//       [Email](#Email)
+//       `
+//     )
+//   }
+//   return ` `
+// };
+
+function renderInstallation(installation){
+  if(installation!=="none"){
+    return(
+      `## Installation Instructions
+      ${installation}
+      `
+    )
+  }
+  return 'No instructions set'
+};
+
+function renderTests(tests){
+  if(tests!=="none"){
+    return(
+      `## Tests
+      ${tests}`
+    )
+  }
+  return ' '
+};
+
+function renderContribution(contribution){
+  if(contribution!=="none"){
+    return(
+      `## Contribution 
+      ${contribution}
+      `
+    )
+  }
+  return "There are no opportunities for contribution."
+};
+
+function renderUsername(github){
+  if (github!=="none"){
+    return(
+      `## Github
+      Username: ${github}`
+    )
+  }
+  return '## Github'
+};
+
+function renderEmail(email){
+  if (email!=="none"){
+    return(
+      `## Email
+      ${email}`
+    )
+  }
+  return `No email provided`
+};
+
+
+
+
+
 function generateMarkdown(data) {
   return `# ${data.title}
-
+  ${renderDescription(data.description)}
+  ${renderTableOfContents(data.tableOfContents)}
+  ${renderInstallation(data.installation)}
+  ${renderTests(data.tests)}
+  ${renderContribution(data.contribution)}
+  ${renderLicenseBadge(data.license)}
+  ${renderUsername(data.github)}
+  ${renderEmail(data.email)}
 `;
 }
 
